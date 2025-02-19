e1f897fa48b43577ac4a0eb62420a2a28cdf0675
Adrian Aro <aroadrianchristopher@gmail.com>
2025-02-19 23:57:35 +0800

    create database, insert data on the database

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request
+from init_db import db_connection
+import psycopg2
+
+app = Flask(__name__)
+
+#para matawag pala yung function na db_connect sa init_db.py
+db_connection()
+
+
+#Database connection
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        password="admin"  )
+    return conn
+
+
+get_db_connection()
+
+@app.route('/', methods=['GET'])
+def get_book():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM books;')
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return books
+
+if __name__ == "__main__":
+    app.run(debug=True)
+   
+
+#error here cant understand
+# @app.route("/books/", methods=('POST'))
+# def post_book():
+#     if request.method == 'POST':
+#         book = request.get_json()
+#         title = book['title']
+#         author = book['author'] 
+#         pages_num = book['pages_num']
+#         review = book['review']
+        
+
+#         conn = get_db_connection()
+#         cur = conn.cursor()
+#         cur.execute('INSERT INTO books (title, author, pages_num, review)'
+#                     'VALUES (%s, %s, %s, %s)',
+#                     (title, author, pages_num, review))
+#         conn.commit()
+#         cur.close()
+#         conn.close()
---
+++ init_db.py
@@ -0,0 +1,29 @@
+import os
+import psycopg2
+
+def db_connection():
+        conn = psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        password="admin"  
+        
+        )
+        cur = conn.cursor()
+
+        cur.execute('DROP TABLE IF EXISTS books;')
+        cur.execute('CREATE TABLE books (id SERIAL PRIMARY KEY,'
+                    'title VARCHAR(150) NOT NULL,'
+                    'author VARCHAR(50) NOT NULL,'
+                    'pages_num integer NOT NULL,'
+                    'review text,'
+                    'date_added date DEFAULT CURRENT_TIMESTAMP);'
+                    )
+        
+
+        cur.execute('INSERT INTO books (title, author, pages_num, review) VALUES (%s, %s, %s, %s)',
+            ('The Catcher in the Rye', 'J.D. Salinger', 277, 'A great book!'))
+        
+        conn.commit()
+        cur.close()
+        conn.close()
